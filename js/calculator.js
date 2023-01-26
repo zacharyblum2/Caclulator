b958c729637e3460023ac0f83c3d67b5da7d224b
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-26 13:13:34 -0500

    Add 4 basic math operators as functions

---
+++ js/calculator.js
@@ -0,0 +1,17 @@
+// Create Add, Subtact, Multiply, and Divide functions.
+
+const add = function(n1, n2) {
+    return n1 + n2;
+}
+
+const subtract = function(n1, n2) {
+    return n1 - n2;
+}
+
+const multiply = function(n1, n2) {
+    return n1 * n2;
+}
+
+const divide = function(n1, n2) {
+    return n1 / n2;
+}
